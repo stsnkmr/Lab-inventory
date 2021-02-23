650c9df54d7df8f4a29479232bce45773de452de
Satoshi Nakamura <xie_xie_pandaa@yahoo.co.jp>
2021-02-23 23:17:14 +0900

    fix bug.

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -77,6 +77,9 @@
                                             <button type="button" class="btn btn-primary">編集</button>
                                         </a>
                                         <div style="display: inline-flex">
+                                            @if ($loop->first) //変更予定 一時的にこれで
+                                                <form action=""></form>
+                                            @endif
                                             {{ Form::open(['method' => 'delete', 'url' => action('BookController@destroy', [$book])]) }}
                                             {{ Form::submit('削除', ['class' => 'btn btn-danger']) }}
                                             {{ Form::close() }}
